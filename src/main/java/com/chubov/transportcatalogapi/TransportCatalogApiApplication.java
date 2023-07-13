4f926fd9cbbfac0d015388febad8b5ddd7540dc6
4bv_di <chubov2002@gmail.com>
2023-07-13 16:56:59 +0300

    Init project

---
+++ src/main/java/com/chubov/transportcatalogapi/TransportCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.chubov.transportcatalogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chubov/transportcatalogapi/TransportCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chubov.transportcatalogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
